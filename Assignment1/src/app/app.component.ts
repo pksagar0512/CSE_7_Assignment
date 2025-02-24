6b5cbba22e422507b63f6f7420e7048c0013c1d3
Jay Deshmukh <jaydeshmukh583@gmail.com>
2025-02-24 22:20:25 +0530

    added angular assignment

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './components/user/user.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { EmployeeComponent } from './components/employee/employee.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserComponent, AdminComponent, EmployeeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
