0027b9e55bf1d3b86d563055769a95bdcd457a93
Rahul Jangir <rahuljangir4368@gmail.com>
2025-02-26 15:40:15 +0530

    update someting

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -3,19 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { UserComponent } from './user/user.component';
 import { AdminComponent } from './admin/admin.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    EmployeeComponent,
-    UserComponent,
-    AdminComponent
-  ],
+  imports: [RouterOutlet, EmployeeComponent, UserComponent, AdminComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Assignment1';
+  activeComponent: string = '';
+
+  showComponent(component: string) {
+    this.activeComponent = component;
+  }
 }
