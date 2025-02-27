5ef19a2f7b54b4ce680e85e57b6758d68efa8029
achal <achalt2814@gmail.com>
2025-02-27 15:59:51 +0530

    done

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AdminComponent,EmployeeComponent,UserComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
+
 })
 export class AppComponent {
   title = 'Assignment1';
