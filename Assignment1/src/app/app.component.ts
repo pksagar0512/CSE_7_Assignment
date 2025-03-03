8046d5aff3422d1fa7d588395fe26db9d8198150
Dilkhush Choudhary <dilkhushchoudhary@Dilkhushs-MacBook-Air.local>
2025-03-03 14:04:50 +0530

    Added Navigation bar

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 import { UserComponent } from './user/user.component';
 import { AdminComponent } from './admin/admin.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EmployeeComponent, UserComponent, AdminComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- Assignment1/src/app/app.routes.ts
+++ Assignment1/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { SigninComponent } from './signin/signin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+    },{
+        path: 'about',
+        component: AboutComponent,
+    },{
+        path: 'contact',
+        component: ContactComponent,
+        },{
+            path: 'signin',
+            component: SigninComponent,
+
+    }
+];
