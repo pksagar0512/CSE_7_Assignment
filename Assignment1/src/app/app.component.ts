9c42c5925f923e459871611a9f31fbc9943ccb67
Suthar Mahesh M <maheshsuthar242@gmail.com>
2025-03-03 14:08:28 +0530

    add the navbar and the components home ,contact etc

--- Assignment1/src/app/app.component.ts
+++ Assignment1/src/app/app.component.ts
@@ -2,15 +2,14 @@ import { Component } from '@angular/core';
 import { EmployeeComponent } from './employee/employee.component';
 import { UserComponent } from './user/user.component';
 import { AdminComponent } from './admin/admin.component';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    EmployeeComponent,  // <app-employee>
-    UserComponent,      // <app-user>
-    AdminComponent      // <app-admin>
-  ],
+  imports: [ RouterOutlet, RouterLink, RouterLinkActive
+ ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- Assignment1/src/app/app.routes.ts
+++ Assignment1/src/app/app.routes.ts
@@ -2,9 +2,26 @@ import { Routes } from '@angular/router';
 import { AdminComponent } from './admin/admin.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { UserComponent } from './user/user.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 export const routes: Routes = [
-  { path: '', component: EmployeeComponent },
   { path: 'user', component: UserComponent },
   { path: 'admin', component: AdminComponent },
+
+  { path:'home',
+    component: HomeComponent
+  },
+  {path: 'about',
+    component:AboutComponent
+  },
+  {path:'contact',
+    component:ContactComponent
+  },
+  { path:'signin',
+    component: SignInComponent
+
+  }
 ];
